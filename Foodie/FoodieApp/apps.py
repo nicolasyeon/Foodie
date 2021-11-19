cd3f83b9175ee37e1bd2794e7ba69ec0f0d9cc06
Nicolas <nkyeon0709@gmail.com>
2021-11-19 11:47:51 -0800

    initial commit

---
+++ Foodie/FoodieApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FoodieappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'FoodieApp'
---
+++ Foodie/FoodieApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('register', views.register),
+    path('login', views.login),
+    path('dashboard', views.dashboard),
+    path('logout', views.logout),
+    path('new_review/<int:user_id>', views.new_review),
+    path('create_review/<int:user_id>', views.create_review),
+    path('view_review/<int:review_id>', views.view_review),
+    path('edit_review/<int:review_id>', views.edit_review),
+    path('remove_review/<int:review_id>', views.remove_review),
+    path('view_account/<int:user_id>', views.view_account),
+    path('edit_account/<int:user_id>', views.edit_account),
+    path('like/<int:review_id>', views.like),
+    path('unlike/<int:review_id>', views.unlike),
+]
